88bbf5b80357ddaba1e7a1b0196c0424d44fc5b1
yoyota <smarthorse6@gmail.com>
2019-10-06 19:37:35 +0900

    Change github api response body to camelcase keys

--- src/app.js
+++ src/app.js
@@ -1,20 +1,22 @@
-/* eslint-disable camelcase */
 const axios = require('axios')
+const camelcaseKeys = require('camelcase-keys')
 
 const hub = axios.create({
   baseURL: 'https://api.github.com',
   headers: { Authorization: `token ${process.env.TOKEN}` }
 })
-hub.interceptors.response.use((response) => response.data)
+hub.interceptors.response.use((response) => {
+  return camelcaseKeys(response.data, { deep: true })
+})
+
+function filterKeys({ id, fullName }) {
+  return { id, fullName }
+}
 
 async function listRepo() {
   const repos = await hub.get('/user/repos?type=owner')
   const activeRepos = repos.filter(({ archived }) => !archived)
-  const repoFullnames = activeRepos.map(({ id, full_name }) => ({
-    id,
-    full_name
-  }))
-  console.log(repoFullnames)
+  return activeRepos.map(filterKeys)
 }
 
 function deleteRepo(repo) {
@@ -24,3 +26,10 @@ function deleteRepo(repo) {
 function archiveRepo(repo) {
   hub.patch(`/repos/huhuta/${repo}`, { archived: true })
 }
+
+async function main() {
+  const repos = await listRepo()
+  console.log(repos)
+}
+
+main()
